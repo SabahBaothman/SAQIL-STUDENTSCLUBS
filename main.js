5aa3ada661e58e5f04b31cf9edb033268b01af34
SabahBaothman <sadbullahbaothman@stu.kau.edu.sa>
2023-04-04 04:01:40 +0300

    SAQIL_first-commit

---
+++ main.js
@@ -0,0 +1,26 @@
+// change navbar
+
+window.addEventListener('scroll',()=>{
+    document.querySelector('nav').classList.toggle('window-srcoll',window.scrollY>0)
+})
+
+const inputs = document.querySelectorAll(".input");
+
+
+function addcl(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+}
+
+function remcl(){
+	let parent = this.parentNode.parentNode;
+	if(this.value == ""){
+		parent.classList.remove("focus");
+	}
+}
+
+
+inputs.forEach(input => {
+	input.addEventListener("focus", addcl);
+	input.addEventListener("blur", remcl);
+});
